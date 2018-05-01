f27f846fde522f93d8a44936a2551c50a7014420
Schlitzkus_Rowan <schlr284@hsd.k12.or.us>
2018-05-01 15:18:36 -0700

    Added barrel. Don't break plz

--- Steampunk/src/main/java/com/kinpai/steampunk/init/ModBlocks.java
+++ Steampunk/src/main/java/com/kinpai/steampunk/init/ModBlocks.java
@@ -17,4 +17,5 @@ public class ModBlocks
 	public static final Block COPPER_BLOCK = new CopperBlock("copper_block", Material.IRON);
 	public static final Block STEAM_BLOCK = new BlockBase("steam_block", Material.IRON);
 	public static final Block FURNACE = new BlockBase("furnace", Material.IRON);
+	public static final Block BARREL = new BlockBase("barrel", Material.WOOD);
 }
